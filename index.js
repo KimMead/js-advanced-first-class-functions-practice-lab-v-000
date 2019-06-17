4d2707c842d29fc8bf513b31e8c7dec50b35e817
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-17 02:01:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file!
-const logDriverNames = function (driver){
+const logDriverNames = function (drivers){
   drivers.forEach(function(element){
     console.log(element.name);
   });
