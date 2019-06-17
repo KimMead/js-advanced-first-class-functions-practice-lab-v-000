ded22fee8baebf689b327d8653e6f6a1e8e7ebc1
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-17 02:11:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ const logDriversByHometown = function (drivers, hometown){
 
 const driversByRevenue = function (drivers){
   return [...drivers].sort(function(a, b){
-    return a.name > b.name;
+    return a.revenue > b.revenue;
   });
 };
